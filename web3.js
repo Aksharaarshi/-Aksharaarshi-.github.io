f55991ab72f089ee2cc429c3382925f959c6605d
Aksharaarshi <154582897+Aksharaarshi@users.noreply.github.com>
2023-12-22 23:36:37 +0530

    Create web3.js

---
+++ web3.js
@@ -0,0 +1,20 @@
+// Example JavaScript for functionality
+
+// Function to display available courses
+function displayCourses() {
+    // Fetch courses from a backend API or create them dynamically
+    // Display course information in #courses section
+  }
+  
+  // Function to display available tutors
+  function displayTutors() {
+    // Fetch tutors from a backend API or create them dynamically
+    // Display tutor information in #tutors section
+  }
+  
+  // Event listener when the page loads
+  document.addEventListener('DOMContentLoaded', function() {
+    // Load available courses and tutors when the page loads
+    displayCourses();
+    displayTutors();
+  });
